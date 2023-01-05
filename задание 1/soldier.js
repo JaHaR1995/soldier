e7e5ea7c5982e5566ebd979905c87bb318afd7ff
Yakumov <dgahar.yakumov@gmail.com>
2023-01-05 12:08:26 +0300

    Я сделал вторую подзадачу

--- задание 1/soldier.js
+++ задание 1/soldier.js
@@ -2,26 +2,45 @@
 
 const soldier = {
 name: 'Ali',  
-health: '10',
+health: 10,
 weapon:{
     name: 'M4',
     cartridges: 30,
 },
 supplies: 3,
 
-fire:() => {},
-recharge:() =>{},
-hurt:() =>{},
+fire: function () {
+    this.cartridges.weapon -1
+  
+    if(this.weapon.cartridges === 0){
+        console.log('обойма пуста Перезарядитесь')
+    }
+    return console.log('бах-бах ')
+    },
 
+recharge: function (){
+    this.weapon.cartridges === 30 && this.supplies -1
 
+    if(this.supplies === 0){
+        console.log('Не осталось припасов')
+    }
+    return console.log('Перезарядка')
+},
+hurt:() =>{
+    this.health - 1
 
-
-
-
-
-
-
-
-
+    if(this.health === 0){
+      
+    }
+    return console.log('Ты проиграл')
 
 },
+
+
+
+
+
+
+
+}
+soldier.hurt()

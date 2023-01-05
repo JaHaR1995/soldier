b4e848197a1fa5119548bf27ab54ecc1ce37c4b8
Yakumov <dgahar.yakumov@gmail.com>
2023-01-05 10:47:43 +0300

    Я сделал подзадачу

---
+++ задание 1/soldier.js
@@ -0,0 +1,27 @@
+
+
+const soldier = {
+name: 'Ali',  
+health: '10',
+weapon:{
+    name: 'M4',
+    cartridges: 30,
+},
+supplies: 3,
+
+fire:() => {},
+recharge:() =>{},
+hurt:() =>{},
+
+
+
+
+
+
+
+
+
+
+
+
+},
